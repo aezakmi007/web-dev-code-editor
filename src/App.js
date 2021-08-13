b3bea54a667a3f2198df0eb41efe4dacbe0129ed
aezakmi007 <abdullahfarhan207@gmail.com>
2021-08-13 16:40:09 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Iframe from "react-iframe";
+import Editor from "./Components/Editor";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui three column grid App">
+      <div className="row">
+        <div className="column">
+          {" "}
+          <Editor />
+        </div>
+        <div className="column">
+          {" "}
+          <Editor />
+        </div>
+        <div className="column">
+          <Editor />
+        </div>
+      </div>
+
+      <div className="row">
+        <Iframe></Iframe>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Editor.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Editor() {
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <textarea value="Code Editor" rows="20"></textarea>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Editor;
